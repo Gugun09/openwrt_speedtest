a499090af376eca147ca375f5f1aefa910e3d681
Nanda Gunawan <nandakanaksasak@gmail.com>
2021-02-12 04:46:54 +0700

    first commit

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# By gugun
+
+# ==================================================
+# Install Speedtest
+cd /usr/bin
+wget -O speedtest "https://raw.githubusercontent.com/Gugun09/openwrt_speedtest/main/speedtest"
+chmod +x speedtest
+cd
+# ==================================================
+clear
+echo "Installasi Selesai..."
+sleep 2
+echo "Menunggu..."
+sleep 1
+echo "Finish..."
